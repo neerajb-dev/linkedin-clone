ce342134fb98fabdd4a7c8d1d3d377220878e462
Neeraj Bhardwaj <neerajb@yahoo.com>
2022-07-18 01:46:19 +0530

    working on left side card component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,58 +3,81 @@ import { Outlet } from "react-router-dom";
 
 const Header = (props) => {
     return (
-        <Container>
-            <Content>
-                <Logo>
-                    <a href="/home">
-                        <img src="/images/home-logo.svg" alt="" />
-                    </a>
-                </Logo>
-                <Search>
-                    <div>
-                        <input type="text" placeholder="Search" />
-                    </div>
-                    <SearchIcon>
-                        <img src="/images/search-icon.svg" alt="" />
-                    </SearchIcon>
-                </Search>
-                <Nav>
-                    <NavListWrap>
-                        <NavList className="active">
-                            <a>
-                                <img src="/images/nav-home.svg" alt="" />
-                                <span>Home</span>
-                            </a>
-                        </NavList>
-                        <NavList>
-                            <a>
-                                <img src="/images/nav-network.svg" alt="" />
-                                <span>My Network</span>
-                            </a>
-                        </NavList>
-                        <NavList>
-                            <a>
-                                <img src="/images/nav-jobs.svg" alt="" />
-                                <span>Jobs</span>
-                            </a>
-                        </NavList>
-                        <NavList>
-                            <a>
-                                <img src="/images/nav-messaging.svg" alt="" />
-                                <span>Messaging</span>
-                            </a>
-                        </NavList>
-                        <NavList>
-                            <a>
-                                <img src="/images/nav-notifications.svg" alt="" />
-                                <span>Notifications</span>
-                            </a>
-                        </NavList>
-                    </NavListWrap>
-                </Nav>
-            </Content>
-            {/* <Outlet /> */}
-        </Container>
+        <>
+            <Container>
+                <Content>
+                    <Logo>
+                        <a href="/home">
+                            <img src="/images/home-logo.svg" alt="" />
+                        </a>
+                    </Logo>
+                    <Search>
+                        <div>
+                            <input type="text" placeholder="Search" />
+                        </div>
+                        <SearchIcon>
+                            <img src="/images/search-icon.svg" alt="" />
+                        </SearchIcon>
+                    </Search>
+                    <Nav>
+                        <NavListWrap>
+                            <NavList className="active">
+                                <a>
+                                    <img src="/images/nav-home.svg" alt="" />
+                                    <span>Home</span>
+                                </a>
+                            </NavList>
+                            <NavList>
+                                <a>
+                                    <img src="/images/nav-network.svg" alt="" />
+                                    <span>My Network</span>
+                                </a>
+                            </NavList>
+                            <NavList>
+                                <a>
+                                    <img src="/images/nav-jobs.svg" alt="" />
+                                    <span>Jobs</span>
+                                </a>
+                            </NavList>
+                            <NavList>
+                                <a>
+                                    <img src="/images/nav-messaging.svg" alt="" />
+                                    <span>Messaging</span>
+                                </a>
+                            </NavList>
+                            <NavList>
+                                <a>
+                                    <img src="/images/nav-notifications.svg" alt="" />
+                                    <span>Notifications</span>
+                                </a>
+                            </NavList>
+
+                            <User>
+                                <a>
+                                    <img src="/images/user.svg" alt="" />
+                                    <span>Me</span>
+                                    <img src="/images/down-icon.svg" alt="" />
+                                </a>
+
+                                <SignOut>
+                                    <a>Sign Out</a>
+                                </SignOut>
+                            </User>
+
+                            <Work>
+                                <a>
+                                    <img src="/images/nav-work.svg" alt="" />
+                                    <span>Work</span>
+                                    <img src="/images/down-icon.svg" alt="" />
+                                </a>
+                            </Work>
+
+                        </NavListWrap>
+                    </Nav>
+                </Content>
+            </Container>
+            <Outlet />
+        </>
     )
 };
 
@@ -190,5 +213,42 @@ const NavList = styled.li`
     }
 `;
 
+const SignOut = styled.div`
+    position: absolute;
+    top: 45px;
+    background: white;
+    border-radius: 0 0 5px 5px;
+    width: 100px;
+    height: 40px;
+    font-size: 16px;
+    transition-duration: 167ms;
+    text-align: center;
+    display: none;
+`;
+
+const User = styled(NavList)`
+    a > img {
+        width: 24px;
+        height: 24px;
+        border-radius: 50%;
+    }
+    
+    span {
+        display: flex;
+        align-items: center;
+    }
+
+    &:hover {
+        ${SignOut} {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+    }
+`;
+
+const Work = styled(User)`
+    border-left: 1px solid rgba(0, 0, 0, 0.08);
+`;
 
 export default Header;
